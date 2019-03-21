b9f86d5e1877938e139df821625f8ef5c7e90d1d
suulcoder <ef.proyectomundial@gmail.com>
2019-03-20 19:22:30 -0600

    first commit

---
+++ Association.java
@@ -0,0 +1,23 @@
+/*
+Universidad del valle de Guatemala
+Saul Contreras
+Michele Benvenuto
+Hoja 7*/
+class Association{
+	
+	private englishWord;
+	private spanishWord;
+
+	public Association(String englishWord, String englishWord){
+		this.englishWord=englishWord;
+		this.spanishWord=spanishWord;
+	}
+
+	public String getEnglish(){
+		return this.englishWord;
+	}
+
+	public String getSpanish(){
+		return this.spanishWord;
+	}
+}
